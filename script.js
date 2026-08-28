dc40ac9e4f9ba7cd7529f56c3ce74dd006133ded
zeeeshuu <chessgmzeeshan@gmail.com>
2026-08-28 11:16:41 +0530

    bdayyyy

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const enterBtn = document.getElementById("enterBtn");
+const reveal = document.getElementById("reveal");
 
+enterBtn.addEventListener("click", () => {
+
+    reveal.classList.add("active");
+
+        setTimeout(() => {
+
+                reveal.scrollIntoView({
+                            behavior: "smooth"
+                                    });
+
+                                        }, 500);
+
+                                        });
